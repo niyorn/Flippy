fc26e8e471cc368c377a4e19ffefb9b566c9cbda
niyorn <c.niyornram@hotmail.com>
2019-07-24 12:28:12 +0200

    update: Seperate session item into component

---
+++ src/components/SessionItem.vue
@@ -0,0 +1,87 @@
+<template>
+    <section class="session-item-container">
+        <div class="session-item">
+            <p class="item session">
+                <span class="title">
+                    Session {{index}}: 
+                </span>    
+                <span class="time">
+                    {{sessionStartTime}} - {{sessionEndTime}}
+                </span>
+            </p>
+            <p class="item break">
+                <span class="title">
+                    Break: 
+                </span> 
+                <span class="time">
+                    {{breakStartTime}} - {{breakEndTime}}
+                </span>
+            </p>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'SessionItem',
+        props: {
+            index: {
+                type: Number
+            },
+            sessionStartTime: {
+                type: String,
+                required: true
+            },
+            sessionEndTime: {
+                type: String,
+                required: true
+            },
+            breakStartTime: {
+                type: String,
+                required: true
+            },
+            breakEndTime: {
+                type: String,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .session-item-container {
+        list-style: none;
+
+        .session-item {
+            margin: 1rem;
+            margin-bottom: 4rem;
+            display: grid;
+            grid-template-rows: 1.5fr 1fr;
+
+            .item {
+                padding: 1rem;
+                font-size: 1.4rem;
+                display: flex;
+                flex-direction: column;
+
+                .title {
+                    font-weight: 700;
+                }
+
+                .time {
+                    font-weight: 400;
+                    margin-top: 1rem;
+                }
+            }
+
+            .session {
+                background-color: #9be2fd;
+            }
+
+            .break {
+                background-color: #bef9be;
+                margin-top: 1rem;
+            }
+        }
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,128 +1,140 @@
 <template>
   <main class="home">
-    <form >
+    <form>
       <div>
         <label>
           Sessions
-          <input v-model="sessions" type="number" class="sessions" placeholder="How many sessions do you want to do?">
+          <input v-model="sessions" type="number" step="" class="sessions" placeholder="How many sessions do you want to do?">
         </label>
       </div>
 
       <div>
         <label>
-          Break time
-          <input v-model="breakTime" type="number" min="0" max="60" class=" break-time" placeholder="How long do you want your break?">
+          Break time (min)
+          <input v-model="breakTime" type="number" step="5" min="0" max="60" class=" break-time"
+            placeholder="How long do you want your break?">
         </label>
       </div>
 
       <div>
         <label>
           Start Time
-          <input v-model="startTime" type="time" class="start-time" placeholder="When do you want to start?">
+          <input v-model="startTime" type="time" step="300" class="start-time" placeholder="When do you want to start?">
         </label>
       </div>
 
       <div>
         <label>
           End time
-          <input v-model="endTime" type="time" class="end-time" placeholder="When do you want to stop?">
+          <input v-model="endTime" type="time" step="300" class="end-time" placeholder="When do you want to stop?">
         </label>
       </div>
 
       <button @click.prevent="calculateSessions">Set</button>
     </form>
 
-    <p v-if="enoughTime">You have {{this.sessions}} sessions of {{sessionUnitTime}} minutes</p>
-    <ul v-for="timeFrame in allSessionTimeFrame" :key="timeFrame.id">
-      <li>
-        <p>sessionTime: {{timeFrame.sessionTime.startTime}} - {{timeFrame.sessionTime.endTime}}</p>
-        <p>breakTime: {{timeFrame.breakTime.startTime}} - {{timeFrame.breakTime.endTime}}</p>
-      </li>
-    </ul>
+    <section v-if="enoughTime" class="session-container">
+      <p class="description">You have {{this.sessions}} sessions of {{sessionUnitTime}} minutes</p>
+      <SessionItem v-for="(timeFrame,index) in allSessionTimeFrame" :key="timeFrame.id"
+        :index="index+1"
+        :sessionStartTime='timeFrame.sessionTime.startTime' :sessionEndTime='timeFrame.sessionTime.endTime'
+        :breakStartTime='timeFrame.breakTime.startTime' :breakEndTime='timeFrame.breakTime.endTime' />
+    </section>
   </main>
 </template>
 
 <script>
-import moment from 'moment'
-// @ is an alias to /src
-export default {
-  name: 'home',
-  data() {
-    return {
-      sessions: 0,
-      breakTime: 0,
-      startTime: '',
-      endTime: '',
-      enoughTime: false,
-      sessionUnitTime: 0,
-      allSessionTimeFrame: []
-    }
-  },
-  watch: {
-    sessions() {
-      this.calculateSessions()
+  import moment from 'moment'
+  import SessionItem from '@/components/SessionItem.vue'
+  // @ is an alias to /src
+  export default {
+    name: 'home',
+    components: {
+      SessionItem
     },
-    breakTime() {
-      this.calculateSessions()
-    },
-    startTime() {
-      this.calculateSessions()
-    },
-    endTime() {
-      this.calculateSessions()
-    }
-  },
-  methods: {
-    calculateSessions() {
-      const totalTime = this.calculateTimeDifference()
-      const breakTime = this.calculateBreakTime()
-
-      if(breakTime < totalTime) {
-        this.enoughTime = true
-        const workTime = totalTime - breakTime
-        this.sessionUnitTime = Math.floor(workTime/this.sessions)
-        this.showTimeFrame(this.sessionUnitTime)
+    data() {
+      return {
+        sessions: 4,
+        breakTime: 20,
+        startTime: '10:00',
+        endTime: '14:00',
+        enoughTime: false,
+        sessionUnitTime: 0,
+        allSessionTimeFrame: []
       }
     },
-    //calculateTimeDifferenceInMinute
-    calculateTimeDifference() {
-      const startTime = moment(this.startTime, "HH,mm")
-      const endTime = moment(this.endTime, "HH,mm")
-      //calculate the difference (in milliseconds)
-      const timeDifferenceInMilliseconds = endTime.diff(startTime)
-      const minute = 60*1000
-
-      const timeDifferenceInMinute = timeDifferenceInMilliseconds/minute
-
-      return timeDifferenceInMinute
+    watch: {
+      sessions() {
+        this.calculateSessions()
+      },
+      breakTime() {
+        this.calculateSessions()
+      },
+      startTime() {
+        this.calculateSessions()
+      },
+      endTime() {
+        this.calculateSessions()
+      }
     },
-    calculateBreakTime() {
-      const totalBreakTime = this.breakTime*this.sessions
-      return totalBreakTime
-    },
-    showTimeFrame(sessionUnitTime) {
-       // We need to know when the session need to be started, that's why a start time has been added
-      const time = moment(this.startTime, 'HH:mm')
-      const sessions = this.sessions
-      const allSessionsTimeFrame = []
+    methods: {
+      calculateSessions() {
+        const totalTime = this.calculateTimeDifference()
+        const breakTime = this.calculateBreakTime()
 
-      for(let i = 0; i < sessions; i++) {
-        const session = {
-          sessionTime : {
-            startTime: time.format('HH.mm'),
-            endTime: time.add(sessionUnitTime, 'minutes').format('HH:mm')
-          },
-          breakTime : 
-            { 
+        if (breakTime < totalTime) {
+          this.enoughTime = true
+          const workTime = totalTime - breakTime
+          this.sessionUnitTime = Math.floor(workTime / this.sessions)
+          this.showTimeFrame(this.sessionUnitTime)
+        }
+      },
+      //calculateTimeDifferenceInMinute
+      calculateTimeDifference() {
+        const startTime = moment(this.startTime, "HH,mm")
+        const endTime = moment(this.endTime, "HH,mm")
+        //calculate the difference (in milliseconds)
+        const timeDifferenceInMilliseconds = endTime.diff(startTime)
+        const minute = 60 * 1000
+
+        const timeDifferenceInMinute = timeDifferenceInMilliseconds / minute
+
+        return timeDifferenceInMinute
+      },
+      calculateBreakTime() {
+        const totalBreakTime = this.breakTime * this.sessions
+        return totalBreakTime
+      },
+      showTimeFrame(sessionUnitTime) {
+        // We need to know when the session need to be started, that's why a start time has been added
+        const time = moment(this.startTime, 'HH:mm')
+        const sessions = this.sessions
+        const allSessionsTimeFrame = []
+
+        for (let i = 0; i < sessions; i++) {
+          const session = {
+            sessionTime: {
+              startTime: time.format('HH.mm'),
+              endTime: time.add(sessionUnitTime, 'minutes').format('HH:mm')
+            },
+            breakTime: {
               startTime: time.format('HH.mm'),
               endTime: time.add(this.breakTime, 'minutes').format('HH.mm')
             }
+          }
+          allSessionsTimeFrame.push(session)
         }
-        allSessionsTimeFrame.push(session)
+
+        this.allSessionTimeFrame = allSessionsTimeFrame
       }
-      
-      this.allSessionTimeFrame = allSessionsTimeFrame
     }
   }
-}
 </script>
+
+<style lang="scss" scoped>
+  .session-container {
+    .description {
+      text-align: center;
+    }
+  }
+</style>
